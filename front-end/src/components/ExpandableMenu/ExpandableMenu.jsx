654d95624fd7badb45fcd8755ec3d74f7b74f2f7
Amadeusz Majcher <amadeuszmajcher@gmail.com>
2025-04-26 00:24:01 +0200

    displaying the product list

--- front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
+++ front-end/src/components/ExpandableMenu/ExpandableMenu.jsx
@@ -1,17 +1,25 @@
 import styles from "./ExpandableMenu.module.css";
 import { CATEGORIES } from "../../constants/categories";
-import { NavLink } from "react-router-dom";
+import { NavLink, useParams } from "react-router-dom";
 import ARROW_ICON from "../../assets/arrow.svg";
+
+const PATH_TO_GENDER_NAME = {
+  kobieta: "Kobieta",
+  mezczyzna: "Mężczyzna",
+  dziecko: "Dziecko",
+};
+
 export function ExpandableMenu() {
-  const activePath = "odziez";
+  const params = useParams();
+  const activePath = params.category;
   return (
     <div className={styles.expandableMenu}>
-      <p>Kobieta</p>
+      <p>{PATH_TO_GENDER_NAME[params.gender]}</p>
       <ul>
         {CATEGORIES.map((category) => {
           return (
             <li key={category.path}>
-              <NavLink to={category.path}>
+              <NavLink to={`/${params.gender}/${category.path}`}>
                 {category.categoryName}
                 <img
                   src={ARROW_ICON}
@@ -25,7 +33,9 @@ export function ExpandableMenu() {
                   {category.subCategories.map((subcategory) => {
                     return (
                       <li key={subcategory.path}>
-                        <NavLink to={subcategory.path}>
+                        <NavLink
+                          to={`/${params.gender}/${params.category}/${subcategory.path}`}
+                        >
                           {subcategory.categoryName}
                         </NavLink>
                       </li>
--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -7,7 +7,9 @@ import { Cart } from "./views/Cart/Cart.jsx";
 import { Favourites } from "./views/Favourites/Favourites.jsx";
 import { Layout } from "./components/Layout/Layout.jsx";
 import { MainPage } from "./views/MainPage/MainPage.jsx";
+import { ProductsLists } from "./views/ProductsLists/ProductsLists.jsx";
 import { mainPageLoader } from "./api/mainPageLoader.js";
+import { productListLoader } from "./api/productListLoader.js";
 const router = createBrowserRouter([
   {
     path: "",
@@ -25,6 +27,11 @@ const router = createBrowserRouter([
         path: "/:gender?",
         element: <MainPage />,
         loader: mainPageLoader,
+      },
+      {
+        path: "/:gender/:category/:subcategory?",
+        element: <ProductsLists />,
+        loader: productListLoader,
       },
     ],
   },
